0d694b12ead76f34215fb2df284111ad49d43f9a
Ryan Krawczyk <ryankrawczyk@ryans-macbook.bc.edu>
2018-10-29 16:24:25 -0400

    more annotations

--- src/Huff.java
+++ src/Huff.java
@@ -3,7 +3,7 @@ authors: Nick Hawk & Ryan Krawczyk
 November 4, 2018
  */
 
-// we're going to need some imports --> List, PriorityQueue, HashMap?
+// we're going to need some imports --> PriorityQueue, HashMap, Binary Tree ADT?
 
 public class Huff {
 
