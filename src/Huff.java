2048995ebd37f068df3e80677b0e82f9c1ec3fea
Nick Hawk <hawkn@bc.edu>
2018-11-01 15:43:31 -0400

    merging

--- src/Huff.java
+++ src/Huff.java
@@ -5,10 +5,7 @@ November 4, 2018
 
 import java.util.HashMap;
 import java.util.PriorityQueue;
-import java.io.*;
-
 import java.util.Map;
-import java.util.HashMap;
 import java.io.*;
 import java.util.NoSuchElementException;
 import java.lang.*;
@@ -19,33 +16,26 @@ public class Huff {
   public Map<Character, Integer> getFreqTable(String fileName){
 
     Map<Character, Integer> freqTable = new HashMap<Character, Integer>();
-    File myFile = new File(fileName);
 
-    if (myFile.exists()) { // checks to see if file exists, then proceeds
-      FileReader file = new FileReader(fileName);
+    FileReader file = openInputFile(fileName);
 
-      if (file.read() == -1) { // Checks for empty file ***Check file.length() method/ look for a better alternative
-        throw new NoSuchElementException("EMPTY FILE");
-      }
-
-      while (file.read() != -1) { // Parses through file, mapping each character to frequency
-
-        Character currentChar = Integer.toString(file.read()).charAt(0);
-
-        if (freqTable.containsKey(currentChar)) { // if the key for the character exists, add 1 to frequency
-          Integer freq = freqTable.get(currentChar);
-          freq += 1;
-          freqTable.put(currentChar, freq);
-        }
-        else { // if key does not exist, map it to frequency = 1
-          freqTable.put(currentChar, 1);
-        }
-      }
+    if (file.read() == -1) { // Checks for empty file ***Check file.length() method/ look for a better alternative
+      throw new NoSuchElementException("EMPTY FILE");
     }
-    else {
-      throw new NoSuchElementException("FILE DOES NOT EXIST");
+
+    while (file.read() != -1) { // Parses through file, mapping each character to frequency
+
+      Character currentChar = Integer.toString(file.read()).charAt(0);
+
+      if (freqTable.containsKey(currentChar)) { // if the key for the character exists, add 1 to frequency
+        Integer freq = freqTable.get(currentChar);
+        freq += 1;
+        freqTable.put(currentChar, freq);
+        }
+      else { // if key does not exist, map it to frequency = 1
+        freqTable.put(currentChar, 1);
+      }
     }
     return freqTable;
   }
-
-}
+  }
