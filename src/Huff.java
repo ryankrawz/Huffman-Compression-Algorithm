8bc5b91264e4acecd459dffa40f9981b8c32447e
Ryan Krawczyk <ryankrawczyk@ryans-macbook.bc.edu>
2018-11-01 23:52:04 -0400

    changing data flow

--- src/Huff.java
+++ src/Huff.java
@@ -25,55 +25,8 @@ import java.lang.Object;
 
 public class Huff {
 
-  // This will require restructuring for new frequency table design
-  public Map<Character, Integer> getFreqTable(String fileName){
-
-      Map<Character, Integer> freqTable = new HashMap<Character, Integer>();
-
-      FileReader file = openInputFile(fileName);
-
-      if (file.read() == -1) { // Checks for empty file ***Check file.length() method/ look for a better alternative
-          throw new NoSuchElementException("EMPTY FILE");
-      }
-
-    while (file.read() != -1) { // Parses through file, mapping each character to frequency
-
-          Character currentChar = Integer.toString(file.read()).charAt(0);
-
-          if (freqTable.containsKey(currentChar)) { // if the key for the character exists, add 1 to frequency
-              Integer freq = freqTable.get(currentChar);
-              freq += 1;
-              freqTable.put(currentChar, freq);
-          }
-          else { // if key does not exist, map it to frequency = 1
-              freqTable.put(currentChar, 1);
-          }
-      }
-      return freqTable;
-  }
-
-  // TODO: pseudo-code, unsure if functional
-  public HuffTree (Map<Character, SymbolInfo> freqTable) {
-      PriorityQueue<HuffTree> treePQ = new PriorityQueue<HuffTree>();
-      Character[] keyArray = (Character[]) freqTable.keySet().toArray();
-
-      // Adds a series of one-item binary trees to the priority queue
-      for (int i = 0; i < keyArray.length; i++) {
-          HuffTree huff = new HuffTreeC((char) keyArray[i], freqTable.get(keyArray[i]).frequency());
-          treePQ.add(huff);
-      }
-
-      // While the priority queue has more than one element,
-      // remove first two trees and contruct new tree for the priority queue
-      while (treePQ.size() > 1) {
-          HuffTree t1 = treePQ.poll();
-          HuffTree t2 = treePQ.poll();
-          HuffTree t3 = new HuffTreeC(null, t1.weight() + t2.weight());
-          t3.insert(t1);
-          t3.insert(t2);
-          treePQ.add(t3);
-      }
-      return treePQ.poll();
+    public static void main(String[] args) {
+        SymbolTable table = new SymbolTableC(args[0]);
     }
 
 }
